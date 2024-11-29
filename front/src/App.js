ebd14ea15d0dd8d1a92ed7460766212741d7e199
김재민 <jmkim12@douzone.com>
2024-11-29 09:09:50 +0900

    폴더명 수정

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router';
 import { HashRouter, Link } from 'react-router-dom';
-import StudyPages from './pages/Study/StudyPages';
-import Project from './pages/Project/ProjectPage';
 import Layout from './components/layout/Layout/Layout';
 import './styles/App.css';
 
@@ -11,10 +9,7 @@ const App = () => {
     <div className='app'>
       <HashRouter>
         <Routes>
-          <Route path='/' element={<Layout />}>
-            <Route index element={<StudyPages />} path='study' />
-          </Route>
-          <Route path='/project' element={<Project />}></Route>
+          <Route path='/' element={<Layout />}></Route>
         </Routes>
       </HashRouter>
     </div>
--- front/src/components/layout/Layout/Layout.js
+++ front/src/components/layout/Layout/Layout.js
@@ -1,13 +1,10 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import * as styles from './Layout.module.css';
 
 const Layout = () => {
-  useEffect(() => {
-    console.log('Layout Component 렌더링됨');
-  }, []);
   return (
     <div className={styles.layout}>
       <Header />
