d36ea933dfbb4636db001fa5599c943f83e08191
김재민 <jmkim12@douzone.com>
2024-12-02 09:12:09 +0900

    상품 검색 쿼리 오타 수정

--- front/src/components/Product/Product.js
+++ front/src/components/Product/Product.js
@@ -1,18 +1,16 @@
-import React from "react";
-
+import React from 'react';
 
 const Product = ({ product = {} }) => {
-
-    const handleClick = () => {
-        // product id를 쿼리스트링으로 해서 상세 검색 화면 출력
-    }
-    return (
-        <div className="product" tabIndex={0} onClick={handleClick}>
-            <img src={product.image_url} />
-            <h2>{product.name}</h2>
-            <p>{product.price}</p>
-            <p>{product.description}</p>
-        </div>
-    )
-}
+  const handleClick = () => {
+    // product id를 쿼리스트링으로 해서 상세 검색 화면 출력
+  };
+  return (
+    <div className='product' tabIndex={0} onClick={handleClick}>
+      {/* <img src={product.image_url} /> */}
+      <h2>{product.name}</h2>
+      <p>{product.price}</p>
+      <p>{product.description}</p>
+    </div>
+  );
+};
 export default Product;
--- front/src/components/Product/ProductDetail.js
+++ front/src/components/Product/ProductDetail.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const ProductDetail = ({ product: { } }) => {
-  return <div>
-    <div className='productDetail'>
-      <div>
-
+const ProductDetail = ({ product: {} }) => {
+  return (
+    <div>
+      <div className='productDetail'>
+        <div></div>
       </div>
-    </div>;
+      ;
+    </div>
+  );
 };
-    export default ProductDetail;
+export default ProductDetail;
