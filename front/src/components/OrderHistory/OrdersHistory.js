ef96495f518f4f23e07a961ece366a1daf790eaa
김재민 <jmkim12@douzone.com>
2024-12-04 18:09:53 +0900

    주문 상태에 따른 분기처리 함수 틀

--- front/src/components/OrderHistory/OrdersHistory.js
+++ front/src/components/OrderHistory/OrdersHistory.js
@@ -3,6 +3,7 @@ import { useLocation } from 'react-router-dom';
 import * as styles from './OrdersHistory.module.css';
 
 const OrdersHistory = () => {
+  // 주문 수정이 이루어 지는 곳
   const location = useLocation();
   const orderId = location.state;
   const url = 'http://localhost:8081/';
@@ -20,19 +21,24 @@ const OrdersHistory = () => {
       .catch((error) => console.log(error));
   };
 
+  const handlePaymentClick = () => {
+    console.log('결제 버튼 클릭'); // 결제 로직 추가
+  };
+  const handleGoToPaymentClick = () => {
+    console.log('결제페이지로 이동 버튼 클릭'); // 결제 페이지로 이동하는 로직 추가
+  };
+
+  const handleEditClick = () => {};
+
+  const handleCancelClick = () => {};
+
+  const handleRefundClick = () => {};
+
   useEffect(() => {
     if (orderId) {
       fetchData();
     }
   }, [orderId]);
-
-  useEffect(() => {
-    console.log('데이터 받아옴: ', orders);
-  }, [orders]);
-
-  useEffect(() => {
-    console.log(ordersProducts);
-  }, [ordersProducts]);
 
   return (
     <div className={styles.ordersHistory}>
@@ -60,10 +66,30 @@ const OrdersHistory = () => {
             )}
           </div>
           <div className={styles.orderButton}>
-            {orders.status === '주문대기' && <button>결제</button>}
-            {orders.status === '주문중' && <button>결제</button>}
-            {orders.status === '주문완료' && <button>환불</button>}
-            {orders.status === '주문실패' && <></>}
+            {orders.status === '주문대기' && (
+              <>
+                <button id='btn-payment' onClick={handlePaymentClick}>
+                  결제
+                </button>
+                <button id='btn-edit' onClick={handleEditClick}>
+                  수정
+                </button>
+                <button id='btn-cancel' onClick={handleCancelClick}>
+                  주문취소
+                </button>
+              </>
+            )}
+            {orders.status === '주문중' && (
+              <button id='btn-payment' onClick={handleCancelClick}>
+                주문취소
+              </button>
+            )}
+            {orders.status === '주문완료' && (
+              <button id='btn-go-to-payment' onClick={handleRefundClick}>
+                환불
+              </button>
+            )}
+            {orders.status === '주문취소' && <></>}
           </div>
         </>
       ) : (
