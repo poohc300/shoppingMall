7d2b7bbc6e448e1088eb730ae2aeb30a87ce86d3
김재민 <jmkim12@douzone.com>
2024-12-05 15:16:37 +0900

    상품리스트 빈값 예외처리

--- front/src/components/Product/ProductList.js
+++ front/src/components/Product/ProductList.js
@@ -5,9 +5,13 @@ import * as styles from './Product.module.css';
 const ProductList = ({ products = [] }) => {
   return (
     <div className={styles.productList}>
-      {products.map((product) => {
-        return <ProductItem key={product.id} product={product} />;
-      })}
+      {products ? (
+        products.map((product) => {
+          return <ProductItem key={product.id} product={product} />;
+        })
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
