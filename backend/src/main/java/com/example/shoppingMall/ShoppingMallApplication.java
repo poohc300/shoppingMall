7ae98e223ec003d29253478a68281fe689fcc711
jeremy <poohc300@gmail.com>
2025-03-06 15:29:03 +0900

    수정

--- backend/src/main/java/com/example/shoppingMall/ShoppingMallApplication.java
+++ backend/src/main/java/com/example/shoppingMall/ShoppingMallApplication.java
@@ -9,13 +9,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShoppingMallApplication implements ApplicationRunner {
 	@Value("${jwt.secret}")
-    private String jwtSecret;
+	private String jwtSecret;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppingMallApplication.class, args);
 	}
+
 	@Override
-    public void run(ApplicationArguments args) throws Exception {
-        System.out.println("JWT Secret: " + jwtSecret);
-    }
-} 
+	public void run(ApplicationArguments args) throws Exception {
+		System.out.println("JWT Secret: " + jwtSecret);
+	}
+}
