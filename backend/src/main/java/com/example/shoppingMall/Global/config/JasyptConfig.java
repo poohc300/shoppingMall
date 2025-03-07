cd900d8c8b638b58c413719ddcd3f3c064edc295
jeremy <poohc300@gmail.com>
2025-03-07 11:02:07 +0900

    구축 완료

--- backend/src/main/java/com/example/shoppingMall/Global/config/JasyptConfig.java
+++ backend/src/main/java/com/example/shoppingMall/Global/config/JasyptConfig.java
@@ -1,20 +1,20 @@
 package com.example.shoppingMall.Global.config;
 
-import jakarta.annotation.PostConstruct;
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class JasyptConfig {
 
-
     @Value("${jasypt.encryptor.password}")
-       private String encryptedSecret;
+    private String encryptedSecret;
 
     @Bean
+    @Primary
     public StringEncryptor stringEncryptor() {
         StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
         encryptor.setPassword(encryptedSecret);
